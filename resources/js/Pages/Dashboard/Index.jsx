f21ac93869fbee42df606196d6a8cb249857e05f
miketanaquin <137193747+miketanaquin@users.noreply.github.com>
2024-05-13 08:51:59 +0800

    first commit

--- resources/js/Pages/Dashboard/Index.jsx
+++ resources/js/Pages/Dashboard/Index.jsx
@@ -11,14 +11,15 @@ export default function Dashboard({ notifs }) {
 
     return (
         <div>
+
             <Head title="Dashboard" />
             <Navbar />
-            <section className="bg-gray-100 dark:bg-gray-900 p-3 sm:p-5">
-                <div className="mx-auto max-w-screen-2xl px-4 lg:px-12">
-                    <div className="bg-white dark:bg-gray-800 relative shadow-md sm:rounded-lg">
-                        <div className="justify-items-end justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
-                            <div className="w-full md:w-auto flex flex-col md:flex-row space-y-2 md:space-y-0 items-stretch md:items-center justify-end md:space-x-3 flex-shrink-0">
-                                <Link href={route('dashboard.store')} type="button" className="flex items-center justify-center text-white bg-blue-600 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">
+            <section className="p-3 bg-gray-100 dark:bg-gray-900 sm:p-5">
+                <div className="px-4 mx-auto max-w-screen-2xl lg:px-12">
+                    <div className="relative bg-white shadow-md dark:bg-gray-800 sm:rounded-lg">
+                        <div className="justify-between p-4 space-y-3 justify-items-end md:space-y-0 md:space-x-4">
+                            <div className="flex flex-col items-stretch justify-end flex-shrink-0 w-full space-y-2 md:w-auto md:flex-row md:space-y-0 md:items-center md:space-x-3">
+                                <Link href={route('dashboard.store')} type="button" className="flex items-center justify-center px-4 py-2 text-sm font-medium text-white bg-blue-600 rounded-lg hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">
                                     <svg className="h-3.5 w-3.5 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                                         <path clipRule="evenodd" fillRule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" />
                                     </svg>
@@ -47,19 +48,19 @@ export default function Dashboard({ notifs }) {
                                             <td className="px-4 py-3 overflow-clip">{notif.context}</td>
                                             {/* <td className="px-4 py-3">{notif.created_at}</td> */}
                                             <td className="px-4 py-3">{notif.expired_at}</td>
-                                            <td className="px-4 py-3 flex items-center justify-end">
+                                            <td className="flex items-center justify-end px-4 py-3">
                                                 <button id={notif.id} data-dropdown-toggle={'btn' + notif.id} className="inline-flex items-center p-0.5 text-sm font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none dark:text-gray-400 dark:hover:text-gray-100" type="button">
                                                     <svg className="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                                         <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
                                                     </svg>
                                                 </button>
-                                                <div id={'btn' + notif.id} className="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600">
+                                                <div id={'btn' + notif.id} className="z-10 hidden bg-white divide-y divide-gray-100 rounded shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
                                                     <div className="py-1 text-sm text-gray-700 hover:bg-gray-100">
-                                                        <Link as='button' href={route('dashboard.edit', { id: notif.id })} method="GET" className="block py-2 px-4 "
+                                                        <Link as='button' href={route('dashboard.edit', { id: notif.id })} method="GET" className="block px-4 py-2 "
                                                         >Edit</Link>
                                                     </div>
                                                     <div className="py-1 dark:hover:text-white hover:bg-gray-100">
-                                                        <Link href={route('dashboard.destroy', { id: notif.id })} method="DELETE" as='button' className="block py-2 px-4 text-sm text-gray-700 dark:text-gray-200 ">Delete</Link>
+                                                        <Link href={route('dashboard.destroy', { id: notif.id })} method="DELETE" as='button' className="block px-4 py-2 text-sm text-gray-700 dark:text-gray-200 ">Delete</Link>
                                                     </div>
                                                 </div>
                                             </td>
